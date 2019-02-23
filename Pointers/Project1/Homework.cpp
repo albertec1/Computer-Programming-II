80257ffeb448248bace4ad5ddb5810de0b0d2827
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-02-23 16:07:07 +0100

    Homework update

---
+++ Pointers/Project1/Homework.cpp
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <iostream>
+
+void ex1() {
+	int arr[10];
+	int* p = arr;
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = (int)p + i;
+
+		std::cout << arr[i] << std::endl;
+
+	}
+}
+
+void reverse(int *arr, int sizeOfArr) {
+	for (int i = 9; i >= 0; i--) {
+		std::cout << *(arr + i) << std::endl;
+	}
+}
+void ex3()
+{
+	int arr[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		int j = 0;
+		std::cout << "type an integer:" << std::endl;
+		std::cin >> j;
+		std::cout << "\n" << std::endl;
+		*(arr + i) = j;
+	}
+	reverse(arr, 10);
+}
+
+void absSwap(int* array1, int sizeOfArray1, int* array2, int sizeOfArray2) {
+	
+	for (int i = 0; i < sizeOfArray1; i++) {
+		if (*(array2 + i) >= 0) {
+			*(array1 + i) = *(array2 + i);
+		}
+		if (*(array2 + i) < 0) {
+			*(array1 + i) = (*(array2 + i) * (-1));
+		}
+	}
+
+	std::cout << "array 1" << "        " << "array2" << std::endl;
+	std::cout << "-------" << "        " << "------" << std::endl;
+
+	for (int i = 0; i < sizeOfArray1; i++) {
+		std::cout << *(array1 + i) << "        " << *(array2 + i) << std::endl;
+	}
+}
+
+void ex4() 
+{
+	//int* nums = (int*)malloc(4 * sizeof(int)); Allocates a block of memory of a given size with no initialization.
+	//int* more_nums = (int*)calloc(4, sizeof(int)); Allocates a block of memory of a given size initialized to zero.	int* arr1 = (int*)calloc(20,sizeof(int));	int* arr2 = (int*)malloc(20 * sizeof(int));		for (int i = 0; i < 20; i++) {		std::cout << "type a signed or an unsigned integer \n" << std::endl;		std::cin >> *(arr2 + i);	}		absSwap(arr1, 20, arr2, 20);
+}
+
+
+int main()
+{
+	//ex1();
+	//ex3();
+	ex4();
+
+	system("pause");
+	return 0;
+}
+
--- Pointers/Project1/Source.cpp
+++ Pointers/Project1/Source.cpp
@@ -1,108 +1,108 @@
-#include <iostream>
-
-
-void ex_1()
-{
-	std::cout << "exercise 1" << std::endl;
-	int energy = 100;
-	int	*/*this star means that we take the memory adress of an object and store it into a pointer*/ptr = &energy;
-	*/*this star means that we take the value poited by the pointer*/ptr = 50;
-	std::cout << energy << std::endl;
-
-	//cout --> console output.
-	//std --> namespace that contains many things, such as the 'cout'.
-	std::cout << "\n" << std::endl;
-}
-
-void ex_2(int *x)
-{
-	std::cout << "exercise 2" << std::endl;
-
-	*x = 0;
-
-}
-
-void ex_3()
-{
-	std::cout << "exercise 3" << std::endl;
-
-	int num = 3;
-	int *ptr = &num;
-	std::cout << ptr << std::endl;
-	int *copy = ptr;
-	std::cout << copy << std::endl;
-	std::cout << *copy << std::endl;
-	*copy = 0;
-	std::cout << *copy << std::endl;
-
-	std::cout << "\n" << std::endl;
-}
-
-void ex_4()
-{
-	std::cout << "exercise 4" << std::endl;
-
-	int vec[3] = { 1, 2, 3 };
-	int *ptr1 = &vec[0];
-	std::cout << ptr1 << std::endl;
-	std::cout << *ptr1 << std::endl;
-
-	std::cout << "\n" << std::endl;
-}
-
-void ex_5()
-{
-	std::cout << "exercise 5" << std::endl;
-
-	int vec[3] = { 1, 2, 3 };
-	int *p = vec;
-	for (int i = 0; i < 3; ++i) {
-		std::cout << p[i] << std::endl;
-	}
-	std::cout << "\n" << std::endl;
-}
-
-void ex_6(int *p)
-{
-	int result = 0;
-	for (int i=0; i < 3; i++)
-	{
-		result += *(p+i);
-	}
-	std::cout << "result: " << result << std::endl;
-}
-
-void ex_7(const char *pointer)
-{
-	while (*pointer != '\0') //size en aleman es mierda
-	{
-		std::cout << *pointer << std::endl;
-		*(pointer++);
-	}
-}
-
-
-int main()
-{
-	ex_1();
-
-	int val = 100;
-	ex_2(&val);
-	std::cout << val << std::endl;
-
-	ex_3();
-	ex_4();
-	ex_5();
-
-	int array[3] = { 1, 2, 3 };
-	int *pointer = array;
-	ex_6(pointer);
-
-	const char string[] = "Hello World of pointers";
-	const char *pts = string;
-	ex_7(pts);
-
-
-	system("pause");
-		return 0;
-}
+//#include <iostream>
+//
+//
+//void ex_1()
+//{
+//	std::cout << "exercise 1" << std::endl;
+//	int energy = 100;
+//	int	*/*this star means that we take the memory adress of an object and store it into a pointer*/ptr = &energy;
+//	*/*this star means that we take the value poited by the pointer*/ptr = 50;
+//	std::cout << energy << std::endl;
+//
+//	//cout --> console output.
+//	//std --> namespace that contains many things, such as the 'cout'.
+//	std::cout << "\n" << std::endl;
+//}
+//
+//void ex_2(int *x)
+//{
+//	std::cout << "exercise 2" << std::endl;
+//
+//	*x = 0;
+//
+//}
+//
+//void ex_3()
+//{
+//	std::cout << "exercise 3" << std::endl;
+//
+//	int num = 3;
+//	int *ptr = &num;
+//	std::cout << ptr << std::endl;
+//	int *copy = ptr;
+//	std::cout << copy << std::endl;
+//	std::cout << *copy << std::endl;
+//	*copy = 0;
+//	std::cout << *copy << std::endl;
+//
+//	std::cout << "\n" << std::endl;
+//}
+//
+//void ex_4()
+//{
+//	std::cout << "exercise 4" << std::endl;
+//
+//	int vec[3] = { 1, 2, 3 };
+//	int *ptr1 = &vec[0];
+//	std::cout << ptr1 << std::endl;
+//	std::cout << *ptr1 << std::endl;
+//
+//	std::cout << "\n" << std::endl;
+//}
+//
+//void ex_5()
+//{
+//	std::cout << "exercise 5" << std::endl;
+//
+//	int vec[3] = { 1, 2, 3 };
+//	int *p = vec;
+//	for (int i = 0; i < 3; ++i) {
+//		std::cout << p[i] << std::endl;
+//	}
+//	std::cout << "\n" << std::endl;
+//}
+//
+//void ex_6(int *p)
+//{
+//	int result = 0;
+//	for (int i=0; i < 3; i++)
+//	{
+//		result += *(p+i);
+//	}
+//	std::cout << "result: " << result << std::endl;
+//}
+//
+//void ex_7(const char *pointer)
+//{
+//	while (*pointer != '\0') //size en aleman es mierda
+//	{
+//		std::cout << *pointer << std::endl;
+//		*(pointer++);
+//	}
+//}
+//
+//
+//int main()
+//{
+//	ex_1();
+//
+//	int val = 100;
+//	ex_2(&val);
+//	std::cout << val << std::endl;
+//
+//	ex_3();
+//	ex_4();
+//	ex_5();
+//
+//	int array[3] = { 1, 2, 3 };
+//	int *pointer = array;
+//	ex_6(pointer);
+//
+//	const char string[] = "Hello World of pointers";
+//	const char *pts = string;
+//	ex_7(pts);
+//
+//
+//	system("pause");
+//		return 0;
+//}
