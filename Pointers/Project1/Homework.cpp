3bd11d8e034ddd8a781a6cb3f9d899775853a2ba
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-02-23 16:16:47 +0100

    Homework ex4 fixed

--- Pointers/Project1/Homework.cpp
+++ Pointers/Project1/Homework.cpp
@@ -34,7 +34,7 @@ void ex3()
 }
 
 void absSwap(int* array1, int sizeOfArray1, int* array2, int sizeOfArray2) {
-	
+
 	for (int i = 0; i < sizeOfArray1; i++) {
 		if (*(array2 + i) >= 0) {
 			*(array1 + i) = *(array2 + i);
@@ -48,22 +48,36 @@ void absSwap(int* array1, int sizeOfArray1, int* array2, int sizeOfArray2) {
 	std::cout << "-------" << "        " << "------" << std::endl;
 
 	for (int i = 0; i < sizeOfArray1; i++) {
-		std::cout << *(array1 + i) << "        " << *(array2 + i) << std::endl;
+		std::cout << *(array1 + i) << "              " << *(array2 + i) << std::endl;
 	}
 }
 
-void ex4() 
+void ex4()
 {
 	//int* nums = (int*)malloc(4 * sizeof(int)); Allocates a block of memory of a given size with no initialization.
-	//int* more_nums = (int*)calloc(4, sizeof(int)); Allocates a block of memory of a given size initialized to zero.	int* arr1 = (int*)calloc(20,sizeof(int));	int* arr2 = (int*)malloc(20 * sizeof(int));		for (int i = 0; i < 20; i++) {		std::cout << "type a signed or an unsigned integer \n" << std::endl;		std::cin >> *(arr2 + i);	}		absSwap(arr1, 20, arr2, 20);
+	//int* more_nums = (int*)calloc(4, sizeof(int)); Allocates a block of memory of a given size initialized to zero.
+
+	int* arr1 = (int*)calloc(20, sizeof(int));
+	int* arr2 = (int*)malloc(20 * sizeof(int));
+
+	for (int i = 0; i < 20; i++) {
+		std::cout << "type a signed or an unsigned integer \n" << std::endl;
+		std::cin >> *(arr2 + i);
+	}
+
+	absSwap(arr1, 20, arr2, 20);
+	free(arr1);
+	free(arr2);
+	arr1 = arr2 = nullptr;
 }
+
 
 
 int main()
 {
 	//ex1();
 	//ex3();
-	ex4();
+	//ex4();
 
 	system("pause");
 	return 0;
