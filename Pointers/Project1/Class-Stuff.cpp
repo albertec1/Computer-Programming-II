218b7dadf830a3a9e823186072290f1fad3a011d
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-03-04 12:33:51 +0100

    HOMEWORK

---
+++ Pointers/Project1/Class-Stuff.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+class Point {
+	private:
+		int x, y;
+
+	public:
+		point(  )
+};
+
+
+int main()
+{
+
+	Point p;
+ 
+	std::cout << p.getx() << " " << p.gety() << std::endl; // Compiles OK (same)
+
+	system("pause");
+	return 0;
+}
